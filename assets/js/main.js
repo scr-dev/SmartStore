22d35a1da231a9d92b245f7b66533e3011c493de
Mario Elvio <81795443+jukerah@users.noreply.github.com>
2022-09-29 23:54:58 -0300

    corrected script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,83 +1,84 @@
-import { Produto } from "./produto.js";
+const ProductList = [];
+let currentId = 0;
+let productId = 0;
 
+const productName = document.getElementById('produto');
+const productPrice = document.getElementById('preco');
+const saveButton = document.getElementById('save-button');
 
-const produto = new Produto();
-
-let btnAdcionarProduto = document.querySelector('#salvar');
-let btnClearform = document.querySelector('#cleanForm');
-
-btnAdcionarProduto.addEventListener('click', salvar);
-btnClearform.addEventListener('click', produto.cleanForm)
-
-
-function salvar() {
-    let produtos = produto.adcionar();
-
-    let cadastroProductInfo = document.querySelector('#produto_cadastrado');
-    let nomeProdutoInvalido = document.querySelector('#nome_produto_invalido');        
-    let precoProdutoInvalido =  document.querySelector('#preco_invalido'); 
-    let produtoDuplicado = document.querySelector('#produto_duplicado');
-    cadastroProductInfo.style.display = "none";
-    produtoDuplicado.style.display = 'none';
-
-   if(produto.validaCampos(produtos)) {
-        produto.novoProduto(produtos)
-       
-        produto.cleanForm();
-        cadastroProductInfo.style.display = "block";
-        nomeProdutoInvalido.style.display = "none";
-        precoProdutoInvalido.style.display = "none";
-   }   
-   listarProduto();
-   console.log(produto.arrayProdutos)
+const cleanForm = () => {
+  productName.value = '';
+  productPrice.value = '';
 }
 
-function listarProduto() {
-   let corpoTabela = document.querySelector('#tbody');
-   corpoTabela.innerHTML = '';
+const showProductList = () => {
+  const bodyTabela = document.querySelector('#tbody');
+  bodyTabela.innerHTML = '';
 
-   for(let i in produto.arrayProdutos){
-       let tr = corpoTabela.insertRow();
-
-       let td_id = tr.insertCell();
-       let td_produto = tr.insertCell();
-       let td_preco = tr.insertCell();
-       let td_acao = tr.insertCell();
-
-       /* Iserindo os dados de forma dinamica  */
-
-       td_id.innerHTML = produto.arrayProdutos[i].id;
-       td_produto.innerHTML = produto.arrayProdutos[i].nomeProduto;
-       td_preco.innerHTML = produto.arrayProdutos[i].valorProduto;
-
-       /*  Alinhando as informaçoes na tabela  */
-
-       td_id.classList.add('center');
-       td_acao.classList.add('center');
-
-       /*  Criando os icones de ações  */
-
-       let iconEdit = document.createElement('i');
-       let iconDelet = document.createElement('i');
-
-        /*  Adcionando as clases dos icones  */
-
-        iconEdit.classList.add('"uil','uil-edit','table__icon');
-        iconDelet.classList.add('uil','uil-times','table__icon');
-
-        /*  Adcionando o elemnto criado ao campo da ações da tabela  */
-
-        td_acao.appendChild(iconEdit);
-        td_acao.appendChild(iconDelet);
-
-       /*  Adcionando a funçao deletar dinamicamente  */
-       iconDelet.addEventListener("click", auxilioDeletar)
-
-       function auxilioDeletar() {        
-         let id = produto.arrayProdutos[i].id        
-         produto.deletar(id)
-       }      
-   }
+  ProductList.map(product => {
+    bodyTabela.innerHTML += `
+      <td>${product.id}</td>
+      <td>${product.name}</td>
+      <td>R$ ${product.price}</td>
+      <td>
+        <img
+          onclick="editProductButton(${product.id - 1}, '${product.name}', ${product.price})"
+          src="assets/img/edit-dark.svg"
+          alt="Editar"
+        >
+        <img
+          onclick="deleteProduct(${product.id})"
+          src="assets/img/delete-dark.svg"
+          alt="Deletar"
+        >
+      </td>
+    `
+  });
 }
 
+const saveProduct = () => {
+  const id = productId;
+  const product = productName.value;
+  const price = productPrice.value;
+  console.log(id);
 
+  ProductList[id].name = product;
+  ProductList[id].price = price;
+
+  showProductList();
+  cleanForm();
+
+  saveButton.innerHTML = 'Cadastrar';
+  saveButton.setAttribute("onclick", "addProduct()");
+}
+
+const editProductButton = (id, product, price) => {
+  saveButton.innerHTML = 'Salvar';
+  saveButton.setAttribute("onclick", "saveProduct()");
+
+  productIid = id;
+  productName.value = product;
+  productPrice.value = price;
+}
+
+const deleteProduct = (id) => {
+  const index = ProductList.findIndex((product) => product.id === id);
+  ProductList.splice(index, 1);
+  showProductList();
+  console.log(ProductList);
+}
+
+const addProduct = () => {
+  currentId += 1;
+  
+  ProductList.push({
+    id: currentId,
+    name: productName.value,
+    price: Number(productPrice.value).toFixed(2)}
+  );
+  
+  showProductList();
+  cleanForm();
+
+  console.log(ProductList);
+}
